56018056808628ee6c98d4c9f80841b4b377c319
Marco Rinck <romulus23@gmail.com>
2013-02-08 20:40:36 +0100

    noch 2 globale "use strict" entfernt

--- app/js/controllers/homeController.js
+++ app/js/controllers/homeController.js
@@ -1,6 +1,6 @@
-'use strict';
-
 define(['app'], function (app) {
+	'use strict';
+
 	return app.controller('HomeController', ['$scope', '$http', 'authService',
 		function HomeController($scope, $http, authService) {
 			
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,8 +1,8 @@
-'use strict';
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('my app', function() {
+'use strict';
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
