69dcb8983de037d0e488d2abdb04792c72ed14a3
Marco Rinck <romulus23@gmail.com>
2013-02-12 21:02:43 +0100

    tests are run now, but fail because of missing app code

--- test/unit/applicationTest.js
+++ test/unit/applicationTest.js
@@ -1,49 +1,44 @@
-define(['angular'], function(angular) { 
-    describe("Sample Project: Testing Modules", function() {
-    describe("MyApp Module:", function() {
+define(['angular', 'app'], function (angular, module) {
+	describe("Sample Project: Testing Modules", function () {
+		describe("MyApp Module:", function () {
 
-      var module;
-      beforeEach(function () {
-        module = angular.module("App");
-      });
-      
-      it("should be registered", function() {    
-        expect(module).not.toBeUndefined();
-      });
+			it("should be registered", function () {
+				expect(module).not.toBeUndefined();
+			});
 
-      describe("Dependencies:", function() {
-        
-        var dependencies;
+			describe("Dependencies:", function () {
 
-        var hasModule = function(module) {
-          return dependencies.indexOf(module) >= 0;
-          console.log(dependencies);
-        };
+				var dependencies;
 
-        beforeEach(function() {
-          dependencies = module.value('app').requires;          
-        });
+				var hasModule = function (module) {
+					return dependencies.indexOf(module) >= 0;
+					console.log(dependencies);
+				};
 
-        it("should have app.filter as a dependency", function() {
-          expect(hasModule('app.filter')).toEqual(true);
-        });
+				beforeEach(function () {
+					dependencies = module.value('myApp').requires;
+				});
 
-        it("should have app.service as a dependency", function() {
-          expect(hasModule('app.service')).toEqual(true);
-        });
+				it("should have app.filter as a dependency", function () {
+					expect(hasModule('app.filter')).toEqual(true);
+				});
 
-        it("should have app.filter as a dependency", function() {
-          expect(hasModule('app.filter')).toEqual(true);
-        });
+				it("should have app.service as a dependency", function () {
+					expect(hasModule('app.service')).toEqual(true);
+				});
 
-        it("should have app.directives as a dependency", function() {
-          expect(hasModule('app.directive')).toEqual(true);
-        });
+				it("should have app.filter as a dependency", function () {
+					expect(hasModule('app.filter')).toEqual(true);
+				});
 
-        it("should have app.provider as a dependency", function() {
-          expect(hasModule('app.provider')).toEqual(true);
-        });
-      });
-    });
-  });
+				it("should have app.directives as a dependency", function () {
+					expect(hasModule('app.directive')).toEqual(true);
+				});
+
+				it("should have app.provider as a dependency", function () {
+					expect(hasModule('app.provider')).toEqual(true);
+				});
+			});
+		});
+	});
 });
